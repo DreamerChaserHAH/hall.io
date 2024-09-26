581c7ed80daf50af07150c32a2df3e2b1b0bccce
pohxiangah <yeohpohxiang@gmail.com>
2024-09-26 17:03:55 +0800

    small updates

--- src/main/java/com/hallio/admin/User.java
+++ src/main/java/com/hallio/admin/User.java
@@ -1,8 +1,10 @@
 package com.hallio.admin;
 
 import com.hallio.dms.IObject;
+import java.util.LinkedList;
+import java.util.List;
 
-public class User implements IObject {
+public class User extends IObject {
     private int id;
     private String username;
     private String password;
@@ -37,10 +39,18 @@ public class User implements IObject {
     }
 
     @Override
-    public String getAttributesWithIdAsString() {
-        return id + "," + username + "," + password + "," + role + "," +
-                firstName + "," + lastName + "," + phone + "," + email + "," +
-                regDate + "," + lastLogin;
+    public LinkedList<String> getAttributes() {
+        LinkedList<String> attributes = new LinkedList<>();
+        attributes.add(username);
+        attributes.add(password);
+        attributes.add(role);
+        attributes.add(firstName);
+        attributes.add(lastName);
+        attributes.add(phone);
+        attributes.add(email);
+        attributes.add(regDate);
+        attributes.add(lastLogin);
+        return attributes;
     }
 
     @Override
@@ -57,20 +67,19 @@ public class User implements IObject {
             this.email = attributes[7];
             this.regDate = attributes[8];
             this.lastLogin = attributes[9];
+        } else {
+            // Handle error or log warning
         }
-    }
-
-    // Implementing additional methods from IObject
-    @Override
-    public String[] getAttributes() {
-        return new String[]{
-                username, password, role, firstName, lastName, phone, email, regDate, lastLogin
-        };
     }
 
     @Override
     public String getFilePath() {
-        return "users.txt"; // Adjust the path as needed
+        return "users.txt"; // Adjust as necessary
+    }
+
+    @Override
+    protected void loadFromString(List<String> list) {
+
     }
 
     // Getter methods
